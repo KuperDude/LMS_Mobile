a2e5c6f1884388482d435b06a36d14d43317af27
KuperDude <fieldsdev06@gmail.com>
2024-07-02 10:23:54 +0300

    Initial Commit

---
+++ LMS_Mobile/LMS_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMS_MobileApp.swift
+//  LMS_Mobile
+//
+//  Created by MyBook on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LMS_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
